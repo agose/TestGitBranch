8e29b14b1878c01659e36974725f6e408d434996
PC <agosdesu@gmail.com>
2022-10-27 12:13:33 +0700

    buat toast main

--- app/src/main/java/com/example/testgitbranch/MainActivity.kt
+++ app/src/main/java/com/example/testgitbranch/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.testgitbranch
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+     toast()
+    }
+
+    private fun toast(){
+        Toast.makeText(this, "ini agus buat", Toast.LENGTH_LONG).show()
     }
 }
