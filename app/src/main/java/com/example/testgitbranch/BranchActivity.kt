623c7878484b96d5716779708f52a5f0eaa250b5
Rizky F <rizkyfadlyanda@gmail.com>
2022-10-27 12:33:43 +0700

    Tesssssssssss1233

---
+++ app/src/main/java/com/example/testgitbranch/BranchActivity.kt
@@ -0,0 +1,12 @@
+package com.example.testgitbranch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BranchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_branch)
+        val a = "ini rizky"
+    }
+}
