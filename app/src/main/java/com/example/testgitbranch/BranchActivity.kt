068acfdbb1e12a31fb2d318789674327600be358
PC <agosdesu@gmail.com>
2022-10-27 12:33:19 +0700

    buat toast main

---
+++ app/src/main/java/com/example/testgitbranch/BranchActivity.kt
@@ -0,0 +1,13 @@
+package com.example.testgitbranch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BranchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_branch)
+
+        val a = "ini agus"
+    }
+}
